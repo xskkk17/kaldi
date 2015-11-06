4183e359a553357fa99d52dcc3b31169aab5111e
xskkk17 <xskui@mail.ustc.edu.cn>
2015-11-06 15:29:24 +0800

    change timit run scripts

--- egs/timit/s5/run.sh
+++ egs/timit/s5/run.sh
@@ -36,7 +36,8 @@ echo "                Data & Lexicon & Language Preparation                     
 echo ============================================================================
 
 #timit=/export/corpora5/LDC/LDC93S1/timit/TIMIT # @JHU
-timit=/mnt/matylda2/data/TIMIT/timit # @BUT
+#timit=/mnt/matylda2/data/TIMIT/timit # @BUT
+timit=/disk1/skxu/data/TIMIT
 
 local/timit_data_prep.sh $timit || exit 1
 
